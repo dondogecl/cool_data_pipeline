58d662c558ebd9aaf4df54781bfe236c6ea445b9
dondogecl <ale@doges.dev>
2024-04-30 05:31:26 -0400

    extract_sql_full: added query to extractthe full table and tested, added basic exceptions, pending to test and also add useful logging messages

--- extract_sql_full.py
+++ extract_sql_full.py
@@ -2,7 +2,7 @@ import pymysql
 import csv
 import boto3
 import configparser
-import sys, os
+import sys, os, logging
 import credentials
 
 my_config = credentials.read_config('pipeline.conf', 'mysql_config',
@@ -11,7 +11,7 @@ my_config = credentials.read_config('pipeline.conf', 'mysql_config',
 # database variables
 
 hostname = my_config[0]
-port = my_config[1]
+port = int(my_config[1])
 username = my_config[2]
 dbname = my_config[3]
 password = credentials.decode_string(my_config[4]) # this one is b64
@@ -22,10 +22,33 @@ conn = pymysql.connect(host=hostname,
                        user=username,
                        password=password,
                        db=dbname,
-                       port=int(port))
+                       port=port)
 
 if conn is None:
     print('Connection failed')
     sys.exit(1)
 print(f"Connected to {hostname} successfully!")
+
+# extract data
+query = """SELECT * FROM animes"""
+local_filename = 'animes_extract.csv'
+
+try:
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        results = cursor.fetchall()
+except Exception as e:
+    print(f"Error during extraction: {e}")
+    sys.exit(1)
+
+# write to csv
+try:
+    with open(local_filename, 'w', newline='') as f:
+        writer = csv.writer(f, delimiter='|')
+        writer.writerows(results)
+        
+    print('Saved data to local CSV file successfully!')
+except Exception as e:
+    print(f"Error during saving local file: {e}")
+    sys.exit(1)
 
