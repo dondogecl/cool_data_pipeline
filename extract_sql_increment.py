7faa73e529ab48589a340ed139c6bae54fd74a4c
dondogecl <ale@doges.dev>
2024-05-02 03:15:59 -0400

    load_to_s3: changed functions into class, to handle the s3 client and methods; extract_sql_increment: added upload of mysql extract to s3 bucket using default values

--- extract_sql_increment.py
+++ extract_sql_increment.py
@@ -4,7 +4,7 @@ import boto3
 import psycopg2
 import pymysql
 import sys, os, logging
-import credentials
+import credentials, load_to_s3
 
 my_config = credentials.read_config('pipeline.conf', 'aws_creds',
                                     ['database', 'username', 'hostname',
@@ -106,13 +106,13 @@ def create_table_animes(conn: psycopg2.extensions.connection) -> None:
     else:
         print(f"Error in creating/connecting to the table animes")
     
-def get_latest_updated_rs(conn):
+def get_latest_updated_rs(conn) -> tuple:
     """Returns the latest updated date in the RS cluster"""
     # define query
     query = """SELECT COALESCE(MAX(last_updated), '1900-01-01') 
             FROM animes;"""
     # fetch result
-    res = execute_query(conn, query, 'one')
+    res = (execute_query(conn, query, 'one'),)
     return res
 
 def connect_to_mysql():
@@ -137,23 +137,64 @@ def connect_to_mysql():
         print(f"Connected to {hostname} successfully!")
     return conn
 
-
+def save_to_csv(filename, results, path) -> str:
+    """ Saves the results of a query to a CSV file 
+    params:
+        filename: name of the file
+        results: results of the query (information to be saved)
+        path: path of the file
+    """
+    output = os.path.join(path, filename)
+    try:
+        with open(output, 'w', newline='') as f:
+            writer = csv.writer(f, delimiter='|')
+            writer.writerows(results)
+            
+        print('Saved data to local CSV file successfully!')
+        return output
+    except Exception as e:
+        print(f"Error during saving local file: {e}")
+        sys.exit(1)
 
 def main():
     # execute connection and terminate if it fails
     conn = connect_to_aws_redshift()
-
     create_table_animes(conn)
-
     latest_update_rs = get_latest_updated_rs(conn)
 
     if latest_update_rs is not None:
-        print(f"Last update: {latest_update_rs}")
+        print(f"Last update: {latest_update_rs[0][0]} (｡◕‿‿◕｡)")
     else:
-        print("Failed to get last update")
+        print("Failed to get last update (╥﹏╥)")
 
-    # close connection
+    # close redshift connection
     conn.close()
+
+    # connect to mysql
+    conn_mysql = connect_to_mysql()
+    # get data last updated from mysql
+    extract_query = f""" SELECT *
+                        FROM animes
+                        WHERE last_updated > '{latest_update_rs[0][0]}';"""
+    
+    output_folder = 'data'
+    filename_increment = 'animes_increment.csv'
+    mysql_results = execute_query(conn_mysql, extract_query, 'all')
+    # dump the results to a csv file
+    csv_file_path = save_to_csv(filename_increment, mysql_results, output_folder)
+    target_filename = filename_increment
+    # close mysql connection
+    conn_mysql.close()
+
+    # upload file to s3
+    s3 = load_to_s3.S3Client(load_to_s3.access_key, load_to_s3.secret_key)
+    bucket = load_to_s3.bucket_name
+    upload_status = s3.upload_to_s3(csv_file_path, filename_increment,
+                                     bucket)
+
+    # Check upload status and print a message in a single line
+    print(f"File '{target_filename}' {'uploaded' if upload_status else 'failed to upload'} to S3 '{bucket}' successfully!")
+
 
 if __name__ == "__main__":
     main()
--- load_to_s3.py
+++ load_to_s3.py
@@ -1,4 +1,3 @@
-import pymysql
 import csv
 import boto3
 import configparser
@@ -10,16 +9,39 @@ aws_s3_config = credentials.read_config('pipeline.conf', 'aws_boto_credentials',
 access_key, secret_key = credentials.decode_credentials(aws_s3_config[0], aws_s3_config[1])
 bucket_name = aws_s3_config[2]
 
-# s3 upload
-s3 = boto3.client('s3', 
-                  aws_access_key_id=access_key, 
-                  aws_secret_access_key=secret_key)
+def initialize_s3(access_key, secret_key):
+    try:
+        s3 = boto3.client('s3', 
+                    aws_access_key_id=access_key, 
+                    aws_secret_access_key=secret_key)
+    except Exception as e:
+        print(f"Error during s3 initialization: {e}")
+        sys.exit(1)
+    return s3
 
-local_filename = 'data/animes_extract.csv'
-s3_file = local_filename
 
-try:
-    s3.upload_file(local_filename, bucket_name, s3_file)
-    print("Upload Successful")
-except Exception as e:
-    print(f"Upload Failed, details: {e}")
+class S3Client:
+    def __init__(self, access_key, secret_key) -> None:
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.client = self._initialize_s3_client()
+
+    def _initialize_s3_client(self) -> boto3.client:
+        return boto3.client(
+            's3', aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret_key)
+
+    def get_client(self) -> boto3.client:
+        return self.client
+    
+    def upload_to_s3(self, local_filename, s3_filename, bucket_name) -> bool:
+        """Uploads a file to an S3 bucket"""
+        try:
+            self.client.upload_file(local_filename, bucket_name, s3_filename)
+            print("Upload Successful")
+            return True
+        except Exception as e:
+            print(f"Upload Failed, details: {e}")
+            return False
+
+
